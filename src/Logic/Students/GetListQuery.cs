d299db9d9db43671c4791ee0f7902a29c9ce7b7c
Geert Huls <geert.huls82@gmail.com>
2019-01-28 22:35:24 +0100

    Improve code cohesion and discoveribility
    - nesting command and handlers
    - move handlers into seperate files

---
+++ src/Logic/Students/GetListQuery.cs
@@ -0,0 +1,55 @@
+﻿using Api.Dtos;
+using Logic.Students;
+using Logic.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic.AppServices
+{
+    public sealed class GetListQuery : IQuery<List<StudentDto>>
+    {
+        public string EnrolledIn { get; }
+        public int? NumberOfCources { get; }
+
+        public GetListQuery(string enrolledIn, int? numberOfCources)
+        {
+            EnrolledIn = enrolledIn;
+            NumberOfCources = numberOfCources;
+        }
+
+        internal sealed class GetListQueryHandler : IQueryHandler<GetListQuery, List<StudentDto>>
+        {
+            private readonly SessionFactory _sessionFactory;
+
+            public GetListQueryHandler(SessionFactory sessionFactory)
+            {
+                _sessionFactory = sessionFactory;
+            }
+
+            public List<StudentDto> Handle(GetListQuery query)
+            {
+                var unitOfWork = new UnitOfWork(_sessionFactory);
+                return new StudentRepository(unitOfWork)
+                    .GetList(query.EnrolledIn, query.NumberOfCources)
+                    .Select(x => ConvertToDto(x))
+                    .ToList();
+            }
+
+            private StudentDto ConvertToDto(Student student)
+            {
+                return new StudentDto
+                {
+                    Id = student.Id,
+                    Name = student.Name,
+                    Email = student.Email,
+                    Course1 = student.FirstEnrollment?.Course?.Name,
+                    Course1Grade = student.FirstEnrollment?.Grade.ToString(),
+                    Course1Credits = student.FirstEnrollment?.Course?.Credits,
+                    Course2 = student.SecondEnrollment?.Course?.Name,
+                    Course2Grade = student.SecondEnrollment?.Grade.ToString(),
+                    Course2Credits = student.SecondEnrollment?.Course?.Credits,
+                };
+            }
+        }
+    }
+}
---
+++ src/Logic/Students/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Logic.AppServices
+{
+    public interface ICommandHandler<in TCommand> where TCommand : ICommand
+    {
+        Result Handle(TCommand command);
+    }
+}
